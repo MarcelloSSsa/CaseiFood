fc29829d06f604398a5ea373a8e1175aab8675a2
Cristhian Juniti Suematsu <suematsu01@gmail.com>
2018-11-21 21:02:26 -0200

    API interface initial implementation

---
+++ app/src/main/java/caseifood/app/com/caseifood/api/OpenWeatherMapApi.kt
@@ -0,0 +1,13 @@
+package caseifood.app.com.caseifood.api
+
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface OpenWeatherMapApi {
+
+    @GET("/data/2.5/weather/city?lat={lat}&lon={lon}")
+    fun getCurrentWeatherByLocation(latitude: Double,
+                             longitude: Double): Call<ResponseBody>
+
+}
